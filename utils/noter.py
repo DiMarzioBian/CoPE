9aa526f42bcbacf8c55801ad1ae7504b5e70e42d
DiMarzio Bian <326361233@qq.com>
2022-12-23 04:29:19 +0800

    updated

--- utils/noter.py
+++ utils/noter.py
@@ -16,7 +16,7 @@ class Noter(object):
         self.alpha_jump = args.alpha_jump
 
         self.f_log = join(args.path_log, time.strftime('%m-%d-%H-%M-', time.localtime()) + args.data + '-gpu' +
-                          args.cuda + str(args.alpha_jump) + '.txt')
+                          args.cuda + '-' + str(args.alpha_jump) + '.txt')
 
         if os.path.exists(self.f_log):
             os.remove(self.f_log)  # remove the existing file if duplicate
@@ -39,8 +39,7 @@ class Noter(object):
     def log_brief(self):
         msg = f'\n[Info] Experiment (dataset:{self.dataset}, cuda:{self.cuda}) ' \
               f'\n\t| lr {self.lr:.0e} | weight_decay {self.weight_decay:.0e} | alpha_jump {self.alpha_jump:.0e} |\n'
-        print(msg)
-        self.write(msg)
+        self.log_msg(msg)
 
     # save args into log file
     def save_args(self):
@@ -53,29 +52,26 @@ class Noter(object):
     def log_train(self, loss, loss_rec, loss_jump, t_gap):
         msg = (f'\t| train | loss {loss:.4f} | loss_rec {loss_rec:.4f} | loss_jump {loss_jump:.4f} '
                f'| time {t_gap:.1f}s |')
-        print(msg)
-        self.write(msg)
+        self.log_msg(msg)
 
     # print and save valid phase result
     def log_valid(self, loss, loss_rec, loss_jump, recall, mrr):
         msg = (f'\t| valid | loss {loss:.4f} | loss_rec {loss_rec:.4f} | loss_jump {loss_jump:.4f} '
                f'| recall {recall:.4f} | mrr {mrr:.4f} |')
-        print(msg)
-        self.write(msg)
+        self.log_msg(msg)
 
     # print and save test phase result
     def log_test(self, recall, mrr):
         msg = f'\t| test  | recall {recall:.4f} | mrr {mrr:.4f} |'
-        print(msg)
-        self.write(msg)
+        self.log_msg(msg)
 
     # print and save final result
     def log_final_result(self, epoch: int, dict_res: dict):
-        msg = '-' * 10 + f' CoPE experiment ends at epoch {epoch} ' + '-' * 10
+        self.log_msg('-' * 10 + f' CoPE experiment ends at epoch {epoch} ' + '-' * 10)
         self.log_brief()
 
+        msg = ''
         for type_mode, res in dict_res.items():
             msg += f'\n\t| {type_mode} | epoch {res[0]} | recall_test {res[1]:.4f} | mrr_test {res[2]:.4f} |'
         msg += '\n'
-        print(msg)
-        self.write(msg)
+        self.log_msg(msg)
